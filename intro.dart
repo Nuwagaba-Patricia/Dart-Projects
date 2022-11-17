134cd55c9c9aa489afa0c2050d2fdcc882a311a5
Nuwagaba-Patricia <98308578+Nuwagaba-Patricia@users.noreply.github.com>
2022-11-17 13:45:45 +0300

    First commit
    Commit for review

---
+++ intro.dart
@@ -0,0 +1,7 @@
+
+
+int main(){
+
+print("hello patricia");
+    return 0;
+}
---
+++ variable.dart
@@ -0,0 +1,6 @@
+void main() {
+  var name = "Patricia Nuwagaba";
+  var age = 24;
+  print("My name is ${name}");
+  print("my age is ${age}years");
+}
